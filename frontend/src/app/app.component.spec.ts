40123e820ac9c9028c97d1729ae2f732d53297f8
ali-hy <ali.haitham.ymy@gmail.com>
2024-07-17 04:02:20 +0400

    Attempt to fix nginx config

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'oee-fusion-frontend' title`, () => {
+  it(`should have the 'oeelevate-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('oee-fusion-frontend');
+    expect(app.title).toEqual('oeelevate-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, oee-fusion-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, oeelevate-frontend');
   });
 });
